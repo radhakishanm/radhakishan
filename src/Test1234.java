b7a9e0c2c25fb4e715f8208343b5dfec93339b84
radhakishan <radhakishanm555@gmail.com>
2022-01-06 15:18:31 +0530

    first commit

---
+++ src/Test1234.java
@@ -0,0 +1,13 @@
+
+public class Test1234 {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		System.out.println("radhakishan");
+		System.out.println("radhakishan1");
+		System.out.println("radhakishan1");
+	}
+
+}
